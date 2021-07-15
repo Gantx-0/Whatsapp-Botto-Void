089ed4f4f4f6dba97230a4a61592c6d7ee0736c5
Gantx Hckr <82259888+Gantx-0@users.noreply.github.com>
2021-07-15 21:31:43 +0200

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -27,8 +27,8 @@ export default class Command extends BaseCommand {
         parsedArgs.flags.forEach((flag) => (parsedArgs.joined = parsedArgs.joined.replace(flag, '')))
         const pack = parsedArgs.joined.split('|')
         const sticker = new Sticker(buffer, {
-            pack: pack[1] || '👾 𝐇𝐚𝐧𝐝𝐜𝐫𝐚𝐟𝐭𝐞𝐝 𝐅𝐨𝐫 𝐘𝐨𝐮 ',
-            author: pack[2] || '𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭𝐭𝐨 𝐕𝐨𝐢𝐝 👾',
+            pack: pack[1] || '👾 �Gantx/Simp Freakin" Sama ',
+            author: pack[2] || '�I Am So Shy 👾',
             crop: parsedArgs.flags.includes('--stretch')
         })
         await sticker.build()
